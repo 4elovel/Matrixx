33ae0ce6627bdf1ef11a1860027f26904a5987fd
4elovel <marcynakvova007@gmail.com>
2023-04-02 21:12:53 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+        var rand = new Random();
+        Console.WriteLine("Set matrix size: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+
+        int[,] matrix1 = new int[size, size],
+            matrix2 = new int[size, size],
+            result = new int[size, size];
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                matrix1[i, j] = rand.Next(1, 11);
+                matrix2[i, j] = rand.Next(1, 11);
+                result[i, j] = 0;
+            }
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                for (int k = 0; k < size; k++)
+                {
+                    //matrix3[i, j] = matrix3[i, j] + matrix1[i, k] * matrix2[k, j];
+                    result[i, j] += matrix1[i, k] * matrix2[k, j];
+                }
+            }
+        }
+        Console.WriteLine("\n First matrix:");
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write($"{matrix1[i, j]}\t");
+            }
+            Console.WriteLine("\n");
+        }
+        Console.WriteLine("\n Second matrix:");
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write($"{matrix2[i, j]}\t");
+            }
+            Console.WriteLine("\n");
+        }
+        Console.WriteLine("\n Result:");
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write($"{result[i, j]}\t");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
